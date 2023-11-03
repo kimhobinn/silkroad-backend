ed5e5ae751f00c1356a699e14822b50818f4644d
Inderby <youk8253@gmail.com>
2023-11-03 13:14:37 +0900

    test 'occupation' method implement in ApiController

--- src/main/java/com/example/silkroad/controller/ApiController.java
+++ src/main/java/com/example/silkroad/controller/ApiController.java
@@ -1,11 +1,16 @@
 package com.example.silkroad.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
 public class ApiController {
-
+    @GetMapping("/occupation/{job-name}")
+    public String occupation(@PathVariable(name = "job-name") String name){
+        //TODO: server to server 통신 후 객체 생성 및 반환 구현. 지금은 임시로 String 반환
+        return name;
+    }
 }
