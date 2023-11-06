f4b41f50e4b9f478e3b2834b97418bec2b349793
Inderby <youk8253@gmail.com>
2023-11-06 16:00:37 +0900

    add validation check in RoadMapRequest class

--- src/main/java/com/example/silkroad/controller/ApiController.java
+++ src/main/java/com/example/silkroad/controller/ApiController.java
@@ -3,6 +3,7 @@ package com.example.silkroad.controller;
 import com.example.silkroad.dto.RoadMapRequest;
 import com.example.silkroad.dto.RoadMapResponse;
 import com.example.silkroad.service.RoadMapService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,7 +15,7 @@ public class ApiController {
     final RoadMapService roadMapService;
 
     @GetMapping("/occupation")
-    public RoadMapResponse occupation( @RequestParam(required = false) RoadMapRequest request) {
+    public RoadMapResponse occupation(@Valid @RequestParam(required = false) RoadMapRequest request) {
         //TODO: server to server 통신 후 객체 생성 및 반환 구현. 지금은 임시로 String 반환
         //String roadMap = roadMapService.GenerateRoadMapFromPython(request);
         return roadMapService.convertStringToRoadMap("test");
--- src/main/java/com/example/silkroad/dto/RoadMapRequest.java
+++ src/main/java/com/example/silkroad/dto/RoadMapRequest.java
@@ -1,15 +1,22 @@
 package com.example.silkroad.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Range;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class RoadMapRequest {
+    @NotBlank(message = "직업을 입력해주세요")
+    @Size(max = 15, message = "직업 명이 너무 길어요")
+    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-zA-Z]+$", message = "직업명을 정확히 입력해주세요")
     String occupation;
     int period = 1;
     Period periodType = Period.YEAR;
