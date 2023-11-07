55951764c84bddafd7aa6ad80f88daa621a88221
Inderby <youk8253@gmail.com>
2023-11-07 22:08:05 +0900

    add parameter to occupation handler method

--- src/main/java/com/example/silkroad/controller/ApiController.java
+++ src/main/java/com/example/silkroad/controller/ApiController.java
@@ -5,6 +5,7 @@ import com.example.silkroad.dto.RoadMapResponse;
 import com.example.silkroad.service.RoadMapService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -15,7 +16,7 @@ public class ApiController {
     final RoadMapService roadMapService;
 
     @GetMapping("/occupation")
-    public RoadMapResponse occupation(@Valid RoadMapRequest request) {
+    public RoadMapResponse occupation(@Valid @ModelAttribute RoadMapRequest request, BindingResult bindingResult) {
         //TODO: server to server 통신 후 객체 생성 및 반환 구현. 지금은 임시로 String 반환
         //String roadMap = roadMapService.GenerateRoadMapFromPython(request);
         return roadMapService.convertStringToRoadMap("test");
