821b59305630def8383d9b3dddbc35896f46e5eb
Inderby <youk8253@gmail.com>
2023-11-08 11:29:42 +0900

    modified ApiController cause by additon of OutlookService

--- src/main/java/com/example/silkroad/controller/ApiController.java
+++ src/main/java/com/example/silkroad/controller/ApiController.java
@@ -1,7 +1,10 @@
 package com.example.silkroad.controller;
 
+import com.example.silkroad.dto.OutlookRequest;
+import com.example.silkroad.dto.OutlookResponse;
 import com.example.silkroad.dto.RoadMapRequest;
 import com.example.silkroad.dto.RoadMapResponse;
+import com.example.silkroad.service.OutlookService;
 import com.example.silkroad.service.RoadMapService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -13,13 +16,18 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api")
 @RequiredArgsConstructor
 public class ApiController {
-    final RoadMapService roadMapService;
-
+    private final RoadMapService roadMapService;
+    private final OutlookService outlookService;
     @GetMapping("/occupation")
     public RoadMapResponse occupation(@Valid @ModelAttribute RoadMapRequest request, BindingResult bindingResult) {
         //TODO: server to server 통신 후 객체 생성 및 반환 구현. 지금은 임시로 String 반환
         //String roadMap = roadMapService.GenerateRoadMapFromPython(request);
         return roadMapService.convertStringToRoadMap("test");
 
+    }
+
+    @GetMapping("/outlook")
+    public OutlookResponse outlook(@Valid @ModelAttribute OutlookRequest request){
+        return outlookService.convertStringToOutlook("test");
     }
 }
